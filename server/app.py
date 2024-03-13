f0567988ee6b4a0816d6f9ed3378586a9202fa3f
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 13:08:20 +0900

    Test: CORS 오류 해결을 위한 127.0.0.1의 주소도 추가

--- server/app.py
+++ server/app.py
@@ -1,14 +1,22 @@
+# app.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # CORS 설정
+# 허용할 주소
+origins = [
+    "http://localhost:3000",  # Next.js 애플리케이션의 주소
+    "http://127.0.0.1:3000",  # Next.js 애플리케이션의 주소
+]
+
+# CORS 미들웨어 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Next.js
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
