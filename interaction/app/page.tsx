746a06a7206d86c4cc30ad801db9af4e53df4ab7
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 12:01:34 +0900

    Feat: a 태그로 페이지 이동 기능 추가

--- interaction/app/page.tsx
+++ interaction/app/page.tsx
@@ -2,12 +2,24 @@ export default function MainPage() {
   return (
     <main>
       <h1>Project B 인터랙션 점검</h1>
-      <p>01 User</p>
-      <p>02 App Server</p>
-      <p>03 REST API Server</p>
-      <p>04 DB Server</p>
-      <p>05 Storage Server</p>
-      <p>06 Machine Learning Model</p>
+      <h2>
+        <a href="/user">01 User</a>
+      </h2>
+      <h2>
+        <a href="/app">02 App Server</a>
+      </h2>
+      <h2>
+        <a href="/fast">03 REST API Server(FastAPI)</a>
+      </h2>
+      <h2>
+        <a href="/database">04 DataBase Server</a>
+      </h2>
+      <h2>
+        <a href="/storage">05 Storage Server</a>
+      </h2>
+      <h2>
+        <a href="/mlModel">06 Machine Learning Model</a>
+      </h2>
     </main>
-  )
+  );
 }
