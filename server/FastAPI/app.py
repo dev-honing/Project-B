f733613abedc11948820b0d2cfc937f55cc50797
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 18:47:00 +0900

    Fix: 잘못 설정된 localhost 수정

--- server/FastAPI/app.py
+++ server/FastAPI/app.py
@@ -30,4 +30,4 @@ def read_root():
 # FastAPI 서버 실행
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
