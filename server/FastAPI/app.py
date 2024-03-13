7539a472ff9199168f037fe623cf406096bcb5b8
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 17:58:26 +0900

    Refactor: FastAPI 실행 로직을 추가해 간편하게 열도록 수정

--- server/FastAPI/app.py
+++ server/FastAPI/app.py
@@ -27,3 +27,7 @@ app.add_middleware(
 def read_root():
     return {"Greet": "Hello, FastAPI!"}
 
+# FastAPI 서버 실행
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
