6ec079b74c12325ea6a329d246f0e0ff9f5fa881
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 12:53:08 +0900

    Bad: Fetch 로직을 담은 컴포넌트를 렌더링 시도했지만 CORS 오류

--- interaction/app/fast/page.tsx
+++ interaction/app/fast/page.tsx
@@ -1,10 +1,13 @@
 // app/fast/page.tsx
+'use client'
+import FetchFastAPI from '../../components/FetchFastAPI';
 
 const FastAPIPage = () => {
   return (
     <main>
       <h1>03 REST API Server(FastAPI)</h1>
       <h3>FastAPI 테스트</h3>
+      <FetchFastAPI />
     </main>
   );
 }
--- interaction/components/FetchFastAPI.tsx
+++ interaction/components/FetchFastAPI.tsx
@@ -5,3 +5,17 @@ import React, { useEffect, useState } from "react";
 interface Data {
   Hello: string;
 }
+
+const FetchFastAPI = () => {
+  const [data, setData] = useState<Data | null>(null);
+  useEffect(() => {
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if (data === null) return <p>Loading...</p>;
+  return <p>{data.Hello}</p>;
+};
+
+export default FetchFastAPI;
