3bb446471ce80157dac4a7993ae813ce5f67964a
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 12:57:03 +0900

    Feat: CORS 오류 해결을 위한 미들웨어 설정

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # Next.js
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
