f40b914197ea3870bee5ae3aba83d21894d09465
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 11:57:21 +0900

    Feat: 업로드 폼 생성 및 UserPage에 import

--- interaction/app/user/page.tsx
+++ interaction/app/user/page.tsx
@@ -1,8 +1,10 @@
+import UploadForm from '../../components/UploadForm';
 export default function UserPage() {
   return (
     <main>
       <h1>01 User</h1>
       <h3>클라이언트 테스트</h3>
+      <UploadForm />
     </main>
   )
 }
---
+++ interaction/components/UploadForm.tsx
@@ -0,0 +1,12 @@
+// components/UploadForm.tsx
+
+const UploadForm = () => {
+  return (
+    <form>
+      <input type="file" />
+      <button type="submit">Upload</button>
+    </form>
+  )
+}
+
+export default UploadForm
