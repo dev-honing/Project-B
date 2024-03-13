6997a040879bfc46b4c386207efe8e4395f6d7b4
Ho Nyeong <dev.honing@gmail.com>
2024-03-13 11:52:23 +0900

    Setup: Project B 인터랙션 점검을 위한 Next App 세팅

---
+++ interaction/app/page.tsx
@@ -0,0 +1,13 @@
+export default function MainPage() {
+  return (
+    <main>
+      <h1>Project B 인터랙션 점검</h1>
+      <p>01 User</p>
+      <p>02 App Server</p>
+      <p>03 REST API Server</p>
+      <p>04 DB Server</p>
+      <p>05 Storage Server</p>
+      <p>06 Machine Learning Model</p>
+    </main>
+  )
+}
