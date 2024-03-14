f2721cc27ad193659b0af93cd9f6c201a1973100
Ho Nyeong <dev.honing@gmail.com>
2024-03-14 10:08:39 +0900

    Fix: 동일한 포트번호 수정

--- server/Express/db.js
+++ server/Express/db.js
@@ -12,15 +12,14 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 // 데이터베이스 연결 풀을 Express 애플리케이션에 추가
 app.set('dbPool', pool);
 
 // Express 애플리케이션을 실행
-const PORT = 3306;
+const PORT = 3333;
 app.listen(PORT, () => {
   console.log(`서버 ON: http://localhost:${PORT}`);
 });
-
-// 추가적인 동작이 필요한 경우 이곳에 작성할 수 있습니다.
