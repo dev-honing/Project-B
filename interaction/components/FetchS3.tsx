775a37708fbe3fab43628896da2f45e6db97b8d1
Ho Nyeong <dev.honing@gmail.com>
2024-03-14 12:21:52 +0900

    Good: 표에 렌더링 성공

--- interaction/components/FetchS3.tsx
+++ interaction/components/FetchS3.tsx
@@ -3,18 +3,19 @@
 import { useState, useEffect } from 'react';
 
 interface S3Response {
-  objects: string[];
+  Key: string;
+  LastModified: string;
+  Size: number;
 }
 
 const FetchS3 = () => {
-  const [data, setData] = useState<S3Response | null>(null);
+  const [data, setData] = useState<S3Response[] | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:5555/bhn-s3');
         const responseData = await response.json();
-        console.log(responseData); // 데이터 확인
         setData(responseData);
       } catch (error) {
         console.error('Error fetching S3 data:', error);
@@ -28,11 +29,25 @@ const FetchS3 = () => {
     <div>
       <h1>S3 Storage Bucket 목록 보기</h1>
       {data ? (
-        <ul>
-          {data.objects.map((object, index) => (
-            <li key={index}>{object}</li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>파일 이름</th>
+              <th>파일 크기</th>
+              <th>마지막 수정 날짜</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* 데이터가 존재하는지 확인하고 매핑 */}
+            {data.map((object, index) => (
+              <tr key={index}>
+                <td>{object.Key}</td>
+                <td>{object.Size}bytes</td>
+                <td>{object.LastModified}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : (
         <p>로딩 중...</p>
       )}
